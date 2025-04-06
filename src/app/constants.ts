cf734b871f642366d5a899af5046f4e77024680f
josuwon <josuwon3371@gmail.com>
2025-04-06 16:54:02 +0900

    자음 및 모음 선택 기능 추가, hangul-js 라이브러리 통합, 레이아웃 및 메타데이터 수정

---
+++ src/app/constants.ts
@@ -0,0 +1,15 @@
+export const CONSONANTS = {
+  group1: ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ'],
+  group2: ['ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ'],
+  group3: ['ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'],
+  group4: ['ㄲ', 'ㄸ', 'ㅃ', 'ㅆ', 'ㅉ']
+};
+
+export const VOWELS = {
+  group1: ['ㅏ', 'ㅑ', 'ㅓ', 'ㅕ', 'ㅗ'],
+  group2: ['ㅛ', 'ㅜ', 'ㅠ', 'ㅡ', 'ㅣ'],
+  group3: ['ㅐ', 'ㅒ', 'ㅔ', 'ㅖ', 'ㅘ'],
+  group4: ['ㅙ', 'ㅚ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅢ']
+};
+
+export type InputType = 'consonant' | 'vowel'; 
